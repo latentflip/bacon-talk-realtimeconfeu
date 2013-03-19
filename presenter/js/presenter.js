7d7f319a65e3435a3f1f9714fc52b2bff4886796
Philip Roberts <phil@latentflip.com>
2013-03-19 20:11:37 +0000

    First commit

---
+++ presenter/js/presenter.js
@@ -0,0 +1,51 @@
+var Present = { currentSlide: 0 };
+Present.showSlide = function(slide) {
+  Present.currentSlide = slide;
+  $('.centered').html(Present.slides[Present.currentSlide]);
+  $('.slideCount').html('Slide ' + (Present.currentSlide+1) + ' of ' + Present.slides.length);
+  
+  $('code').attr('data-language', 'javascript');
+  Rainbow.color()
+};
+Present.nextSlide = function() {
+  if (Present.currentSlide < Present.slides.length-1) {
+    Present.showSlide(Present.currentSlide+1);
+  }
+};
+Present.prevSlide = function() {
+  if (Present.currentSlide > 0) {
+    Present.showSlide(Present.currentSlide-1);
+  }
+};
+
+Present.reload = function() {
+    $.ajax({
+        url: 'presentation.md',
+        success: function(data) {
+            if (data.length>0) {
+                converter = new Showdown.converter();
+                var converted = converter.makeHtml(data);
+                Present.slides = converted.split('<p>!</p>');
+                Present.showSlide(Present.currentSlide);
+            }
+        }
+    });
+};
+Present.reload();
+
+$(document).keydown(function(e){
+    if (e.keyCode == 37) {
+       Present.prevSlide();
+       return false;
+    }
+    if (e.keyCode == 39) {
+      Present.nextSlide();
+      return false;
+    }
+    if (e.keyCode == 32) { // space
+        Present.reload();
+        return false;
+    }
+});
+
+
